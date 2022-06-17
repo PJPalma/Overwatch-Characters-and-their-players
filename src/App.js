f07e18afc0e5f2448b0494d069e5508628b12a89
PJPalma <102538053+PJPalma@users.noreply.github.com>
2022-06-17 18:47:54 +0100

    axious try outs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
+import axios from 'axios';
 import Home from './Pages/Home';
+
+//const axios.get('https://best-overwatch-api.herokuapp.com/')
+//.then(function (response) {
+//  console.log(response);
+//})
+
 
 function App() {
   return <div className='Home'> 
