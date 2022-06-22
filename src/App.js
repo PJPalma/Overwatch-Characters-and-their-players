8b148bf7b3a476b7cb87eebb9c1775efffd51847
PJPalma <102538053+PJPalma@users.noreply.github.com>
2022-06-22 01:14:45 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import axios from 'axios';
 import Home from './Pages/Home';
+import React, {useState, useEffect} from 'react'
 
-const URL = ('https://best-overwatch-api.herokuapp.com/')
-axios.all([
-  axios.get(URL)
-])
-.then ((res) => console.log(res))
+
 
 function App() {
+const [player,setPlayer] = useState()
+
+const URL = ('http://owapi.io/profile/pc/us/Jay3-11894')
+useEffect(() => {
+axios.get(URL)
+  .then(function (response) {
+    setPlayer(response)
+    console.log(response);
+  });
+},[]);
+
   return <div className='Home'> 
   <Home/>
+<p>{player?.data?.username}</p>
   </div>
-
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import axios from 'axios'
 
-  
-
-  
-  
 
 function Home(){
   return (
